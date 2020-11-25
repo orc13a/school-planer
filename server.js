33f5e1a76f0eb0ecd422c7d03847f00f2c62bc1f
Oliver Christensen <orc13a@live.dk>
2020-11-25 14:03:57 +0100

    ejs added

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const server = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
+server.set("view engine", "ejs");
+server.set("views", __dirname + "/views");
+server.set("view options", { layout: false } );
+
 server.get('/', (req, res) => {
     res.status(200).json({ messages: "Hello World" });
     res.end();
+});
+
+server.get('/ejs', (req, res) => {
+    res.render('index');
 });
 
 server.listen(port, (err) => {
