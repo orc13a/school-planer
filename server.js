51d06d23649d37583f897cf633bbd72e8d58e7ff
Oliver Christensen <orc13a@live.dk>
2020-11-26 09:15:44 +0100

    add navbar

--- server.js
+++ server.js
@@ -8,14 +8,22 @@ const port = process.env.PORT || 5000;
 server.set("view engine", "ejs");
 server.set("views", __dirname + "/views");
 server.set("view options", { layout: false } );
+server.use(express.static(__dirname + '/public'));
+
+// server.get('*', (req, res) => {
+//     res.status(404).json({ error: "404 not found" });
+//     // res.render('pages/index');
+//     res.end();
+// });
 
 server.get('/', (req, res) => {
-    res.status(200).json({ messages: "Hello World" });
+    res.render('pages/index', { active: 'home' });
     res.end();
 });
 
-server.get('/ejs', (req, res) => {
-    res.render('index');
+server.get('/add', (req, res) => {
+    res.render('pages/add', { active: 'add' });
+    res.end();
 });
 
 server.listen(port, (err) => {
